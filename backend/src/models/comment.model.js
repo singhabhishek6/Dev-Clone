e45de4ba4ff4d7a9094408a4328dbf5605cb0c2c
chandan1499 <gupta.chandan1499@gmail.com>
2021-09-29 01:53:32 +0530

    comments with user and post

---
+++ backend/src/models/comment.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    description: { type: 'string', required: true },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    post_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+        required: true
+    }
+},{
+    versionKey: false,
+    timestamps: true
+})
+
+const Comment = mongoose.model('comment', commentSchema);
+
+module.exports = Comment;
