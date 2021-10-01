ec1f3c1a85712bdc8df1426d2b524fc39d9037ea
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-10-01 15:15:17 +0530

    post completed

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,39 +1,47 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Middle } from '../Components/middle/Middle'
-import { Navbar } from '../Components/Navbar/Navbar'
-import { RightBar } from '../Components/RightBar/RightBar'
-import { SideBar } from '../Components/SideBar/SideBar'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Middle } from "../Components/middle/Middle";
+import { Navbar } from "../Components/Navbar/Navbar";
+import { RightBar } from "../Components/RightBar/RightBar";
+import { SideBar } from "../Components/SideBar/SideBar";
 
 export const HomePage = () => {
-    const [toggle,setToggle] = useState(false)
-    const [login,setLogin] = useState(true)
+  const [toggle, setToggle] = useState(false);
+  const [login, setLogin] = useState(true);
 
-    return (
-        <Home>
-            <Navbar login={login} setLogin={setLogin} setToggle={setToggle} toggle={toggle}/>
-            <div className="wrapper">
-                <SideBar login={login} setLogin={setLogin} setToggle={setToggle}  toggle={toggle}/>
-                <Middle/>
-                <RightBar/>
-            </div>
-        </Home>
-    )
-}
-
+  return (
+    <Home>
+      <Navbar
+        login={login}
+        setLogin={setLogin}
+        setToggle={setToggle}
+        toggle={toggle}
+      />
+      <div className="wrapper">
+        <SideBar
+          login={login}
+          setLogin={setLogin}
+          setToggle={setToggle}
+          toggle={toggle}
+        />
+        <Middle />
+        <RightBar />
+      </div>
+    </Home>
+  );
+};
 
 const Home = styled.div`
- width: 100%;
- background-color: var(--sideCard-color);
- .wrapper{
-     @media screen and (max-width: 1040px) {
-       width  :90%;
-      }
-     width: 92%;
-     max-width: 1250px;
-     margin: auto;
-     margin-top: 15px;
-     display: flex;
-    
- }
-`
+  width: 100%;
+  background-color: var(--sideCard-color);
+  .wrapper {
+    @media screen and (max-width: 1040px) {
+      width: 90%;
+    }
+    width: 92%;
+    max-width: 1250px;
+    margin: auto;
+    margin-top: 15px;
+    display: flex;
+  }
+`;
